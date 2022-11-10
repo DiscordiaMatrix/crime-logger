d4858e6cdcaa3f643441a24905abb51abbfb89d0
DiscordiaMatrix <discordia@rlsh.net>
2022-11-10 11:14:08 -0500

    🚧 Progress toward minimum working state

---
+++ src/views/C_List.vue
@@ -0,0 +1,30 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Crimes List</ion-title>
+      </ion-toolbar>
+      <ion-toolbar>
+        <ion-searchbar></ion-searchbar> // TODO: Add debouncer, and search functionality
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Crimes List</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonSearchbar, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'C_List',
+  components: { IonHeader, IonToolbar, IonSearchbar, IonTitle, IonContent, IonPage }
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Home</ion-title>
+        </ion-toolbar>
+      </ion-header>
+        Welcome to the NYI's mapping project!<br>
+        <br>
+        Please use the menu below to view the logged crimes, or to view the map.<br>
+        <br>
+        You can also add crimes to the database by clicking the "Add Crime" button.
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+
+export default  defineComponent({
+  name: 'Home_Page',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+});
+</script>
